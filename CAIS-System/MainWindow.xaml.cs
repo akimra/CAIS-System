7ed868764a4066205d58363a5fb2bd18f3e85677
Аким Рахимбердиев <rakhimberdiev209@gmail.com>
2019-07-09 13:04:13 +0400

    First commit. Wrap for NodeSmev class

---
+++ CAIS-System/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CAIS_System
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NodeSmev smev = new NodeSmev();
+        private static StringBuilder cmdOutput = null;
+        static string outCmdInfo;
+        Process cmdProcess;
+        StreamWriter cmdStreamWriter;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MainWindow_Load (object sender, EventArgs e)
+        {
+           
+        }
+        private void MainWindow_Dispatch (object sender, EventArgs e)
+        {
+            cmdStreamWriter.Close();
+            cmdProcess.WaitForExit();
+            cmdProcess.Close();
+        }
+        private static void SortOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                cmdOutput.Append(Environment.NewLine + outLine.Data);
+                outCmdInfo = outLine.Data;
+                MessageBox.Show(outCmdInfo);
+            }
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cmdOutput = new StringBuilder("");
+            cmdProcess = new Process();
+
+            cmdProcess.StartInfo.FileName = "cmd.exe";
+            cmdProcess.StartInfo.UseShellExecute = false;
+            cmdProcess.StartInfo.CreateNoWindow = true;
+            cmdProcess.StartInfo.RedirectStandardOutput = true;
+
+            cmdProcess.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
+            cmdProcess.StartInfo.RedirectStandardInput = true;
+            cmdProcess.Start();
+
+            cmdStreamWriter = cmdProcess.StandardInput;
+            cmdProcess.BeginOutputReadLine();
+            cmdStreamWriter.WriteLine("identitytool.bat");
+            
+            smev.SendMessage(outCmdInfo);
+
+        }
+    }
+}
---
+++ CAIS-System/NodeSmev.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Windows;
+using System.Net;
+
+namespace CAIS_System
+{
+    class NodeSmev
+    {
+        private static Uri address = new Uri("http://smev3-n0.test.gosuslugi.ru:7500/smev/v1.2/ws");
+        private static BasicHttpBinding binding = new BasicHttpBinding("SMEVMessageExchangeSoap11Binding");
+        private static EndpointAddress endpoint = new EndpointAddress(address);
+        private static ChannelFactory<TestSmevService.SMEVMessageExchangePortTypeChannel> port = new ChannelFactory<TestSmevService.SMEVMessageExchangePortTypeChannel>(binding, endpoint);
+        private static TestSmevService.SMEVMessageExchangePortTypeChannel channel = port.CreateChannel();
+        private static TestSmevService.SendRequestRequest req = new TestSmevService.SendRequestRequest();
+        public NodeSmev()
+        {
+            channel.Open();
+        }
+        public void SendMessage(string messageId)
+        {
+            string st;
+            System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+            doc.Load("Request0.xml");
+            req.SenderProvidedRequestData = new TestSmevService.SenderProvidedRequestData();
+            req.SenderProvidedRequestData.MessagePrimaryContent = doc.DocumentElement;
+            req.SenderProvidedRequestData.TestMessage = new TestSmevService.Void();
+            req.SenderProvidedRequestData.AttachmentHeaderList = new TestSmevService.AttachmentHeaderList();
+            
+            req.SenderProvidedRequestData.MessageID = messageId;
+            st = Serialize(req);
+            
+            TestSmevService.SendRequestResponse sendreqresp = new TestSmevService.SendRequestResponse();
+            //sendreqresp = channel.SendRequest(req);
+            MessageBox.Show(st);
+        }
+        public string Serialize<TType> (TType sourceObject)
+        {
+            if (sourceObject == null)
+            {
+                return string.Empty;
+            }
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(TType));
+            System.IO.StringWriter stringWriter = new System.IO.StringWriter();
+            using (System.Xml.XmlWriter writer = System.Xml.XmlWriter.Create(stringWriter, new System.Xml.XmlWriterSettings() { Indent = true }))
+            {
+                serializer.Serialize(writer, sourceObject);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
